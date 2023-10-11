ea1b9b53af39152646cdc6452b6c9e23175f1a74
AsadulIslamRipon <mdasadulripon@gmail.com>
2023-10-12 01:16:08 +0600

    adding logger with winston and winston rotate hour file

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
-import mongoose from 'mongoose'
-import config from './config/index'
-import app from './app'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { logger, errorLogger } from './shared/logger';
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
-    console.log(`🛢   Database is connected successfully`)
+    await mongoose.connect(config.database_url as string);
+    logger.info(`🛢   Database is connected successfully`);
 
     app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`)
-    })
+      logger.info(`Application  listening on port ${config.port}`);
+    });
   } catch (err) {
-    console.log('Failed to connect database', err)
+    errorLogger.error('Failed to connect database', err);
   }
 }
 
-main()
+main();
